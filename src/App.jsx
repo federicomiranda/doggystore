b755e1d22b97f48a3da7ff5d40ee9b566abc0374
federicomiranda <federico.mirandaa@gmail.com>
2020-12-10 21:19:13 -0300

    Ejemplo de useState con el input que cambia en tiempo real, super loco

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import {useState} from 'react';
 import './reset.css';
 import NavAndWidgetCart from './components/general/NavAndWidgetCart/NavAndWidgetCart';
 import FeaturedProducts from './components/Home/FeaturedProducts/FeaturedProducts';
 
 function App() {
+  const [nombre, setNombre] = useState('');
+
+  const actualizarNombre = (e) => {
+    setNombre(e.target.value);
+  }
+
   return (
     <>
       <NavAndWidgetCart />
       <FeaturedProducts />
+      <p>{nombre}</p>
+      <input type="text" value={nombre} onChange={actualizarNombre} />
     </>
   );
 }
