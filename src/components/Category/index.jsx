359a5b6964372e30d6adfbcd6f9281b8c0e565c6
federicomiranda <federico.mirandaa@gmail.com>
2021-01-14 19:42:51 -0300

    todas las pantallas consultan por los productos a firestore

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -3,14 +3,24 @@ import {products} from '../../products'
 import {useParams} from 'react-router-dom';
 import ProductsList from './ProductsList';
 import './Category.css';
+import {getFirestore} from '../../db';
 
 const Category = () => {
     const {category_name} = useParams();
     const [prods, setProds] = useState([]);
+    const db = getFirestore();
 
     useEffect(() => {
         if(category_name) {
-            setProds(products.filter(prod => prod.category === category_name))
+            db.collection('productos').where('category', '==', category_name).get()
+            .then(response => {
+                let arr = [];
+                response.forEach(doc => {
+                    arr.push({id: doc.id, data: doc.data()})
+                })
+
+                setProds(arr);
+            })
         }
     }, [category_name])
 
